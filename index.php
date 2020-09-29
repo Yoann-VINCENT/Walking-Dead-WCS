20ff32cefd9c20aa98e5b11661805554011e0794
Yoann PARDO-VINCENT <65440660+Yoann-VINCENT@users.noreply.github.com>
2020-09-29 11:04:57 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+$characters = [
+    "Negan" => [
+        "city" => "The Sanctuary",
+        "weapon" => "Lucille"
+    ],
+    "Daryl" => [
+        "city" => "Alexandria",
+        "weapon" => "crossbow"
+    ],
+    "Ezekiel" => [
+        "city" => "The Kingdom",
+        "weapon" => "Shiva"
+    ],
+    "Yoann VINCENT" => [
+        "city" => "Lyon",
+        "weapon" => "ma chemise (mais c'est mou !)"
+    ],
+];
+
+var_dump($characters);
